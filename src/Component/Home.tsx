81156477a7d29f9905b9c99049c1f9e9fbab9098
alaminsahed <alaminsahed97@gmail.com>
2022-03-19 17:03:00 +0600

    feat: page genareated done

--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -1,4 +1,14 @@
 import React, { useEffect, useState } from "react";
+import {
+  Paper,
+  TableContainer,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+  Pagination,
+  Table,
+} from "@mui/material";
 import axios from "axios";
 
 type InfoType = {
@@ -14,14 +24,7 @@ const Home = () => {
   const [info, setInfo] = useState<InfoType[]>([]);
 
   useEffect(() => {
-    axios
-      .get(
-        `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${page}`
-      )
-      .then((res) => setInfo([...info, res.data.hits]));
-  });
-
-  useEffect(() => {
+    fetchData();
     const autoTiming = setTimeout(() => {
       setPage((prev) => prev + 1);
     }, 1000);
@@ -30,10 +33,52 @@ const Home = () => {
       clearInterval(autoTiming);
     };
   });
-  console.log(info);
+
+  const fetchData = async () => {
+    await axios
+      .get(
+        `https://hn.algolia.com/api/v1/search_by_date?tags=story&page=${page}`
+      )
+      .then((res) => setInfo([...info, ...res.data.hits]));
+  };
+
   return (
     <div>
       <h1>working</h1>
+
+      <Paper>
+        <TableContainer>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {/* <TableCell>Created_at</TableCell> */}
+                <TableCell>Title</TableCell>
+                <TableCell>Author</TableCell>
+                <TableCell>URL</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {info.slice(page * 10, page * 10 + 10).map((value, i) => {
+                return (
+                  <TableRow>
+                    <TableCell>{value.title}</TableCell>
+                    <TableCell>{value.author}</TableCell>
+                    <TableCell>{value.url}</TableCell>
+                    <TableCell>{value.created_at}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+          <Pagination
+            count={page + 1}
+            page={page}
+            color="primary"
+            variant="outlined"
+            shape="rounded"
+          />
+        </TableContainer>
+      </Paper>
     </div>
   );
 };
